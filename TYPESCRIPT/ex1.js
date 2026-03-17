4076c590f99ce40b687bf6986f11fd96dc36845e
kauanne <kauanne.l@escolar.ifrn.edu.br>
2026-03-17 11:51:36 -0300

    ex1

--- TYPESCRIPT/ex1.js
+++ TYPESCRIPT/ex1.js
@@ -4,4 +4,7 @@ let nome = 'Kauanne';
 let matr = 20231144010050;
 let outra = 'Luíza';
 console.log(outra);
+let pessoa; //qualquer tipo
+//undefined: sem valor definido
+let nota; // a barra vai permitir a variãvel ser mais de uma coisa
 //# sourceMappingURL=ex1.js.map
