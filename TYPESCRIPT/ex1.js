e180b60a663bce7d41e77165d8a388a331e983bc
kauanne <kauanne.l@escolar.ifrn.edu.br>
2026-03-17 11:43:16 -0300

    ex1

---
+++ TYPESCRIPT/ex1.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome = 'Kauanne';
+let matr = 20231144010050;
+let outra = 'Luíza';
+console.log(outra);
+//# sourceMappingURL=ex1.js.map
--- TYPESCRIPT/ex1.ts
+++ TYPESCRIPT/ex1.ts
@@ -1,2 +1,4 @@
-let nome = 'Kauanne'
-let matr = 20231144010050
+let nome = 'Kauanne';
+let matr = 20231144010050;
+let outra: string = 'Luíza';
+console.log(outra)
